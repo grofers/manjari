8e7588cc03f9452c95f5bb201fb90a9bb81da58a
manimanjari <manimanjari25@gmail.com>
2016-06-28 10:14:44 +0530

    http response changes

--- doit/views.py
+++ doit/views.py
@@ -8,7 +8,8 @@ from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from authentication.models import Account
 
@@ -24,27 +25,44 @@ class TaskViewSet(viewsets.ModelViewSet):
     '''
     permission_classes = (IsAuthenticated,)
     authentication_classes = (SessionAuthentication, BasicAuthentication)
+    pagination_class = StandardResultsSetPagination
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
     def list(self, request, *args, **kwargs):
         filter_argument = self.request.query_params.get('q')
         date_today = datetime.date.today()
+        response = {}
+        status = {}
+        data = {}
 
         if filter_argument:
                 filter_argument = int(filter_argument)
                 date = date_today - datetime.timedelta(days=filter_argument)
                 queryset = Task.objects.filter(
-                    due_date__date__gte=date,user_id=request.user.id).order_by('due_date')
+                    due_date__date__gte=date,
+                    user_id=request.user.id).order_by('due_date')
         if not filter_argument:
-            queryset = Task.objects.filter(user_id=request.user.id).order_by('-updated_at')
-        
+            queryset = Task.objects.filter(
+                user_id=request.user.id).order_by('-updated_at')
         queryset = self.paginate_queryset(queryset)
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        if serializer:
+            status['success'] = True
+            data['tasks'] = serializer.data
+            response['status'] = status
+            response['data'] = data
+            return Response(response)
+
+        status['success'] = False
+        error = {}
+        error['msg'] = 'Invalid Page'
+        status['error'] = error
+        response['status'] = status
+        return Response(response, status=404)
 
     def create(self, request, *args, **kwargs):
-        serializer = TaskSerializer(data = request.data)
+        serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user_id=Account.objects.get(email=request.user).id)
         return Response(serializer.data)
