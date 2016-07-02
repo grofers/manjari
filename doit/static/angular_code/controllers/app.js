e12f1198d8742fc1473d56d967a08f463caa36ab
manimanjari <manimanjari25@gmail.com>
2016-07-02 23:14:20 +0530

    added create and delete tasks

--- doit/static/angular_code/controllers/app.js
+++ doit/static/angular_code/controllers/app.js
@@ -84,6 +84,7 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
     $scope.task_set = {}
     $scope.query = {}
     $scope.queryBy = '$'
+
     $scope.get_today_tasks = function() {
         $http.get("/api/v1/tasks/?q=1").success(function(response) {
             $scope.task_set = response.data.tasks;
@@ -93,6 +94,7 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
         })
     }
     $scope.get_today_tasks();
+
     $scope.get_week_tasks = function() {
         $http.get("/api/v1/tasks/?q=7").success(function(response) {
             $scope.task_set = response.data.tasks;
@@ -101,24 +103,38 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
             console.log(data);
         })
     }
+
     $scope.get_tasks = function() {
         $http.get("/api/v1/tasks/").success(function(response) {
-            $scope.task_set = response.data.tasks
+            $scope.task_set = response.data.tasks;
+            $scope.delete_value = 3;
         }).error(function(data, status) {
             console.log(data);
         })
     }
+
+    $scope.toggle_showme = function() {
+            $scope.showme = $scope.showme === true ? false: true;
+    };
+
+    $scope.toggle_showedit = function() {
+            $scope.showdit = $scope.showdit === true ? false: true;
+    };
+
     $scope.today = function() {
         return $scope.date = new Date();
     };
     $scope.today();
     $scope.showWeeks = true;
+
     $scope.clear = function() {
         return $scope.date = null;
     };
+
     $scope.disabled = function(date, mode) {
         return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
     };
+
     $scope.toggleMin = function() {
         var _ref;
         return $scope.minDate = (_ref = $scope.minDate) != null ? _ref : {
@@ -132,34 +148,70 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
         return $scope.opened = true;
 
     };
-    $scope.task_add = function(description, date, time) {
-        var dat = $scope.date;
-        var tim = $scope.time;
-        var d = new Date(dat.getFullYear(), dat.getMonth(), dat.getDate(), tim.getHours(), tim.getMinutes());
+      $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+      var currentDay = new Date().setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return 'highlight-current-date'; 
+        }
+    }
+
+    return '';
+  };
+
+    $scope.task_add = function(task) {
+        $scope.today();
+        var task_data =$scope.task;
+        var date = task_data.date;
+        if (date == null){
+          $scope.fullDate = null;
+        }
+        else {
+        var time = task_data.time;
+        var d = new Date(date.getFullYear(), date.getMonth(), date.getDate(), time.getHours(), time.getMinutes());
         $scope.fullDate = d;
+      }
         var data = {
-            "description": $scope.description,
+            "description": task_data.description,
             "due_date": $scope.fullDate,
         };
+        var defaultForm={
+            description : "",
+            date : $scope.today(),
+            time : new Date()
+        }
+        $scope.task_form.$setPristine();
+        $scope.task = angular.copy(defaultForm);
         $http.post("/api/v1/tasks/", data).success(function(data, status) {
-            $scope.shownow = true;
+            $scope.showadd = true;
+            if ($scope.delete_value == 1) {
+                $scope.get_today_tasks();
+            }
+            if ($scope.delete_value == 2) {
+                $scope.get_week_tasks();
+            } 
+            if ($scope.delete_value == 3) {
+                $scope.get_tasks();
+            }
             $timeout(function() {
-                $scope.shownow = false;
-            }, 100000);
-            console.log(data);
+                $scope.showadd = false;
+            }, 500);
         }).error(function(data, status) {
             console.log(data);
         })
     }
 
-    $scope.task_edit = function(id) {
-        $scope.showme = false;
+    $scope.getMyCtrlScope = function() {
+         return $scope;   
     }
+
+
 
     $scope.task_done = function(id) {
 
-    }
-
+    };
     $scope.task_delete = function(id) {
         $http.delete("/api/v1/tasks/" + id).success(function(response) {
             if ($scope.delete_value == 1) {
@@ -167,8 +219,14 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
             }
             if ($scope.delete_value == 2) {
                 $scope.get_week_tasks();
-            } else
+            } 
+            if ($scope.delete_value == 3) {
                 $scope.get_tasks();
+            }
+            $scope.showdel = true;
+            $timeout(function() {
+                $scope.showdel = false;
+            }, 500);
         }).error(function(data, status) {
             console.log(data);
         })
@@ -180,3 +238,45 @@ module.controller("TaskController", function($scope, $http, $filter, $timeout) {
     $scope.time = new Date();
     $scope.ismeridian = true;
 });
+
+module.controller("EditController", function($scope, $http, $timeout) {
+
+    $scope.task_update = function(edit,task) {
+      var new_task = $scope.edit;
+      var id = $scope.task.id;
+      var date = new_task.date;
+      if (date == null) {
+         $scope.fullDate = null;
+      }
+      else {
+      var time = new_task.time;
+      var d = new Date(date.getFullYear(), date.getMonth(), date.getDate(), time.getHours(), time.getMinutes());
+      $scope.fullDate = d;
+    }
+      var data = {
+            "description": new_task.description,
+            "due_date": $scope.fullDate,
+        };
+      $http.put("/api/v1/tasks/"+ id +"/", data).success(function(data, status) {
+            console.log(data);
+
+            if ($scope.delete_value == 1) {
+                $scope.get_today_tasks();
+            }
+            if ($scope.delete_value == 2) {
+                $scope.get_week_tasks();
+            } 
+            if ($scope.delete_value == 3) {
+                $scope.get_tasks();
+            }
+           $timeout(function() {
+                $scope.toggle_showedit();
+            }, 500);
+
+        }).error(function(data, status) {
+            console.log(data);
+        })
+    }
+});
+
+
